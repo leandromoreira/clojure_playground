a57f94dad8dec480754d1a26988d22164294ed2b
Leandro Moreira <leandro.rhcp99@gmail.com>
2011-12-04 14:02:48 -0200

    creating another map using the previous one assoc

--- metadata.clj
+++ metadata.clj
@@ -2,3 +2,6 @@
 (def serializable-mario (with-meta mario {:serializable true}))
 
 (println (str "mario = serializable-mario => " (= mario serializable-mario)))
+(println (meta serializable-mario))
+(def luigui (assoc mario :main-color "Green"))
+(println luigui)
